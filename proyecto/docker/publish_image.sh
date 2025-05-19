dae2914cb0746060e3b55844ba283f7715099625
AndresMunozcuadrado <a.munozm.2022@alumnos.urjc.es>
2025-05-19 15:37:58 +0200

    arreglo cambios

--- proyecto/docker/publish_image.sh
+++ proyecto/docker/publish_image.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-#docker push hhectorgonzlez/lkj:1.0.0
-docker push andresmunoz2/lkj:1.0.0
-=======
 docker push hhectorgonzlez/lkj-casino:1.0.0
 sleep 2
->>>>>>> 3f41af490f51d45eb8824a865153643594e26d81
